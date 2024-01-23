423ecacc49ae52a8df014f7f0bd09cd8e56a6b6d
Fernando Gómez <fernandogomezrecio@gmail.com>
2024-01-23 13:09:56 +0100

    Funcionalidad de validación de input de precio

--- public/bundle.js
+++ public/bundle.js
@@ -23,8 +23,10 @@ button.addEventListener('click', (e) => {
 
 const form = document.querySelector('#formulario-gasto form');
 const description = form.descripcion;
+const price = form.precio;
 
 const expRegDescription = /^[a-zA-Z0-9\_\- ]{4,30}$/;
+const expRegPrice = /^\d+(\.\d+)?$/;
 
 const checkDescription = () => {
     if (!expRegDescription.test(descripcion.value)) {
@@ -46,12 +48,41 @@ const checkDescription = () => {
     }
 };
 
+const checkPrice = () => {
+    if (!expRegPrice.test(precio.value)) {
+        price.classList.add('formulario-gasto__input--error');
+
+        form.precio.parentElement
+            .querySelector('.formulario-gasto__leyenda')
+            .classList.add('formulario-gasto__leyenda--active');
+
+        return false
+    } else {
+        price.classList.remove('formulario-gasto__input--error');
+
+        form.precio.parentElement
+            .querySelector('.formulario-gasto__leyenda')
+            .classList.remove('formulario-gasto__leyenda--active');
+
+        return true
+    }
+};
+
+// EventListener para cuando el input de descripción pierde el focus
 description.addEventListener('blur', (e) => checkDescription());
+// EventListener para cuando el input tiene un error y el usuario comienza a escribir para corregirlo
 description.addEventListener('keyup', (e) => {
     if ([...e.target.classList].includes('formulario-gasto__input--error')) {
         checkDescription();
     }
+});
 
-    // checkDescription()
+// EventListener para cuando el input de precio pierde el focus
+price.addEventListener('blur', (e) => checkPrice());
+// EventListener para cuando el input tiene un error y el usuario comienza a escribir para corregirlo
+price.addEventListener('keyup', (e) => {
+    if ([...e.target.classList].includes('formulario-gasto__input--error')) {
+        checkPrice();
+    }
 });
 //# sourceMappingURL=bundle.js.map
--- src/addExpense.js
+++ src/addExpense.js
@@ -1,7 +1,9 @@
 const form = document.querySelector('#formulario-gasto form')
 const description = form.descripcion
+const price = form.precio
 
 const expRegDescription = /^[a-zA-Z0-9\_\- ]{4,30}$/
+const expRegPrice = /^\d+(\.\d+)?$/
 
 const checkDescription = () => {
     if (!expRegDescription.test(descripcion.value)) {
@@ -23,11 +25,40 @@ const checkDescription = () => {
     }
 }
 
+const checkPrice = () => {
+    if (!expRegPrice.test(precio.value)) {
+        price.classList.add('formulario-gasto__input--error')
+
+        form.precio.parentElement
+            .querySelector('.formulario-gasto__leyenda')
+            .classList.add('formulario-gasto__leyenda--active')
+
+        return false
+    } else {
+        price.classList.remove('formulario-gasto__input--error')
+
+        form.precio.parentElement
+            .querySelector('.formulario-gasto__leyenda')
+            .classList.remove('formulario-gasto__leyenda--active')
+
+        return true
+    }
+}
+
+// EventListener para cuando el input de descripción pierde el focus
 description.addEventListener('blur', (e) => checkDescription())
+// EventListener para cuando el input tiene un error y el usuario comienza a escribir para corregirlo
 description.addEventListener('keyup', (e) => {
     if ([...e.target.classList].includes('formulario-gasto__input--error')) {
         checkDescription()
     }
+})
 
-    // checkDescription()
+// EventListener para cuando el input de precio pierde el focus
+price.addEventListener('blur', (e) => checkPrice())
+// EventListener para cuando el input tiene un error y el usuario comienza a escribir para corregirlo
+price.addEventListener('keyup', (e) => {
+    if ([...e.target.classList].includes('formulario-gasto__input--error')) {
+        checkPrice()
+    }
 })
